2d6f6fe6ebae3b3c5007487a4a0207ef6eed0480
ysgreen89 <bahir2309@gmail.com>
2023-06-15 21:39:40 -0600

    Cambios 14.06.2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,27 @@
+import { Component} from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+public characters: Character[] = [{
+  name: 'Krilin',
+  power: 1000
+},{
+  name: 'Goku',
+  power: 9500
+},{
+  name: 'Vegeta',
+  power: 8500
+}];
+
+onNewCharacter (character: Character): void{
+console.log('MainPage');
+console.log(character);
+}
+
+}
